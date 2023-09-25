73c7f541d331aa60367c24428985843a45d9c3d7
Lucas <sachettimellalucas@gmail.com>
2023-09-25 20:57:03 -0300

    uopa

---
+++ src/Entidades/Triangulo.java
@@ -0,0 +1,17 @@
+package Entidades;
+
+public class Triangulo {
+    private int lado1;
+
+    public Triangulo(int lado1) {
+        this.lado1 = lado1;
+    }
+
+    public int getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(int lado1) {
+        this.lado1 = lado1;
+    }
+}
